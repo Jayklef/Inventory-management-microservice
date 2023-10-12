4cdc8d3b4f848c2a9b450bb052825a81df305b75
Jayklef <jerryarhawho@gmail.com>
2023-10-12 10:23:05 +0100

    OrderService, OrderController and OrderRepository

--- order-service/src/main/java/com/jayklef/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/jayklef/orderservice/controller/OrderController.java
@@ -1,17 +1,22 @@
 package com.jayklef.orderservice.controller;
 
 import com.jayklef.orderservice.dto.OrderDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jayklef.orderservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/orders")
+@RequiredArgsConstructor
 public class OrderController {
 
-    @PostMapping
+    private final OrderService orderService;
+
+    @PostMapping("/place-order")
+    @ResponseStatus(HttpStatus.CREATED)
     public String placeOrder(@RequestBody OrderDetails orderDetails){
+        orderService.placeOrder(orderDetails);
         return "Order placed successfully";
     }
 }
